3a13c586bdbb6fce8a3b6b13912695950afbcb17
Adriano Gotardo <adrianogotardo.12@gmail.com>
2024-07-08 00:30:44 -0300

    Feat: packed things here at working-sites branch so I can start developing the orders routes

--- src/router/routes/workingSitesRouter.js
+++ src/router/routes/workingSitesRouter.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { roleValidator } from "../../middlewares/roleValidatorMiddleware.js";
-import { registerNewWorkingSite, updateWorkingSiteData } from "../../controllers/workingSitesControllers.js";
+import { registerNewWorkingSite } from "../../controllers/workingSitesControllers.js";
 
 const workingSitesRouter = Router();
 
 workingSitesRouter.use(roleValidator);
-
 workingSitesRouter.post("/", registerNewWorkingSite);
 //workingSitesRouter.get("/all", );
 //workingSitesRouter.get("/:id/details", );
